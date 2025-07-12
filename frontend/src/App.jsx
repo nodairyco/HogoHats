efe1fde3a95fe78f9f2a290f2f1bf26e001bd878
nodairyco <aniashvili.nodar@kiu.edu.ge>
2025-07-13 02:33:46 +0400

    feat(home): added home logic with temporary design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,18 @@ import LoginPage from "./components/registration/LoginPage.jsx";
 import AdminPanel from "./components/adminpanel/AdminPanel.jsx"
 import {createContext, Suspense, useState} from "react";
 
-export const context = createContext()
+export const productContext = createContext()
+
 
 function App() {
 
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+    
 
     return (
         <>
-            <context.Provider value={{products, setProducts}}>
-                <Container sx={{
+            <productContext.Provider value={{products, setProducts}}>
                 <main style={{
                     display: 'flex', height: '100vh', justifyContent: 'center',
                     overflowY:'auto', flexDirection:'column'
--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,13 +1,13 @@
 import React, {useContext, useEffect, useState} from 'react';
 import Cookies from "universal-cookie";
-import {context} from "../../App.jsx";
+import {productContext} from "../../App.jsx";
 import axios from "axios";
-import {ImageList, ImageListItem, useMediaQuery, useTheme} from "@mui/material";
+import {Box, ImageList, ImageListItem, useMediaQuery, useTheme} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
-function Home(props) {
+function Home() {
     const cookies = new Cookies(null, {path: '/'})
-    const {products, setProducts} = useContext(context)
+    const {products, setProducts} = useContext(productContext)
 
     useEffect(() => {
         const fetchProducts = async () => {
@@ -35,7 +35,7 @@ export default Home;
 function MapItems() {
     const theme = useTheme()
 
-    const {products} = useContext(context)
+    const {products} = useContext(productContext)
     const isXs = useMediaQuery(theme.breakpoints.down('sm')); // <600px
     const isSm = useMediaQuery(theme.breakpoints.between('sm', 'md')); // 600–900px
     const isMd = useMediaQuery(theme.breakpoints.between('md', 'lg')); // 900–1200px
@@ -43,19 +43,71 @@ function MapItems() {
     const cols = isXs ? 2 : isSm ? 2 : isMd ? 3 : 4;
 
     return (
-        <ImageList cols={cols} gap={16}>
-            {products.map((product) => (
-                <ImageListItem key={product._id} sx={{backgroundColor: 'red', padding: 1}}>
-                    <img
-                        src={product.images[0].url}
-                        alt={product.name}
-                        loading="lazy"
-                        style={{width: '100%', borderRadius: 8, objectFit: 'crop', maxHeight:'300px', maxWidth:'300px'}}
-                    />
-                    <Typography>{product.name}</Typography>
-                    <Typography>${product.price}</Typography>
-                </ImageListItem>
-            ))}
-        </ImageList>
+
+        <Box sx={{maxWidth: 1200, margin: '0 auto', width: '100%'}}>
+            <ImageList cols={cols} gap={16} sx={{p:12}}>
+                {products.map((product) => (
+                    <ItemCard product={product}/>
+                ))}
+            </ImageList>
+        </Box>
+    )
+}
+
+function ItemCard({product}) {
+    const [isFocused, setIsFocused] = useState(false)
+    
+    const capitalizeProductName = productName => {
+        let lst = productName.split(' ')
+        lst = lst.map(str => `${str.charAt(0).toUpperCase()}${str.substring(1)}`)
+        return lst.join(' ')
+    }
+
+    return (
+        <ImageListItem key={product._id} sx={{
+            padding: 1.5,
+            borderRadius: 2,
+            // backgroundColor: 'red', 
+            transition: 'all 0.3s ease',
+            maxWidth: '300px',
+            '&:hover': {
+                cursor: 'pointer',
+                boxShadow: '0px 4px 35px 0px #A8ACB030',
+                transform: 'translateY(4px)'
+            }
+        }}
+                       onMouseEnter={() => setIsFocused(true)}
+                       onMouseLeave={() => setIsFocused(false)}
+
+        >
+            <img
+
+                src={isFocused ? product.images[1].url : product.images[0].url}
+                alt={product.name}
+                loading="lazy"
+                style={{
+                    width: '100%',
+                    borderRadius: 8,
+                    objectFit: 'cover',
+                    aspectRatio: '1/1',
+                    maxHeight: '276px',
+                    maxWidth: '276px',
+                    transition: 'all 0.3s ease',
+                    marginBottom: '12px'
+                }}
+            />
+            <Typography fontFamily='Raleway, sans-serif' fontWeight='600' fontSize='18px'
+                        sx={{
+                            color: 'white'
+                        }}>
+                {capitalizeProductName(product.name)} 
+            </Typography>
+            <Typography fontFamily='Raleway, sans-serif'
+                        sx={{
+                            color: 'white'
+                        }}>
+                ${product.price}
+            </Typography>
+        </ImageListItem>
     )
 }
