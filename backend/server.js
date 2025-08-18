20984f5db417750f95029e1bd85beb68c79244f0
nodairyco <aniashvili.nodar@kiu.edu.ge>
2025-08-18 12:19:59 +0400

    hopefully final commit for backend hosting

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const orderRoutes = require('./routes/orderRoutes');
 
 // Middleware
 app.use(cors({
-  origin: "*",
+  origin: ["http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-type', 'Authorization', 'Cookie']
