71bc2e56eaf9f168e41a87f026e5b2a260edec09
nodairyco <aniashvili.nodar@kiu.edu.ge>
2025-08-18 17:30:17 +0400

    bacakend fix 2 for frontend hosting

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const orderRoutes = require("./routes/orderRoutes");
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://hogo.vercel.app/"],
+    origin: ["http://localhost:5173", "https://hogo.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-type", "Authorization", "Cookie"],
